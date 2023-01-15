1d63e47278747d97c793fc2a570b847d8324069b
kunori <ykunori1986@gmail.com>
2023-01-15 23:10:23 +0900

    feat

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const {ApolloServer} = require('apollo-server')
 
 const typeDefs = `
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhoto: [Photo!]!
+  }
+  
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
+    category: PhotoCategory!
+    postedBy: User!
+    taggedUsers:[User!]!                                                                                                                                                                                                                                                   
   }
-  
-  
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -33,8 +43,46 @@ const typeDefs = `
   }
 `
 
+const user = [
+  {githubLogin: '111', name: 'aaa'},
+  {githubLogin: '113', name: 'bb'},
+  {githubLogin: '115', name: 'ccc'},
+]
+
+const photos = [
+  {
+    id: '1',
+    name: 'a',
+    description: 'a_description',
+    category: 'ACTION',
+    githubUser: '111'
+  },
+  {
+    id: '2',
+    name: 'b',
+    description: 'b_description',
+    category: 'SELFIE',
+    githubUser: '115'
+  }
+]
+
+const tags = [
+  {
+    photoID: 1, userID: '111'
+  },
+  {
+    photoID: 2, userID: '112'
+  },
+  {
+    photoID: 2, userID: '113'
+  },
+  {
+    photoID: 2, userID: '111'
+  },
+]
+
 let _id = 0
-let photos = []
+// let photos = []
 
 const resolvers = {
   Query: {
@@ -55,7 +103,22 @@ const resolvers = {
   },
 
   Photo: {
-    url: parent => `http://hoge.com/img/${parent.id}.jpg`
+    url: parent => `http://hoge.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return user.find(u => u.githubLogin === parent.githubUser)
+    },
+    taggedUsers: parent => tags.filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    },
+    inPhoto: parent => tags.filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
+
   }
 
 }
